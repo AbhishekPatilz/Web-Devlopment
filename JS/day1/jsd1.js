8835090bb6179f77e6443bf60cb95aa83850a93f
AbhishekPatilz <ap3820214a@gmail.com>
2025-09-09 00:38:51 +0530

    jsd1

---
+++ JS/day1/jsd1.js
@@ -0,0 +1,32 @@
+//different O/p method
+
+// let a = 10;
+// let b = 5;
+// // let c = a + b;
+// console.log(a+b);
+
+console.log("Hello World");
+confirm("take all cooke");
+alert("warning");
+document.writeln("IM Ro fan");
+document.write("45 only");
+
+//inpute
+a = prompt("Enter ur name");
+
+// token-is smallest entity of program
+//token== datatype/keyword + variable/identifire + value/constant
+
+let a = 10;
+let b = 5;
+let c = a + b;
+console.log(a+b);
+
+console.log("Hello World");
+confirm("take all cooke");
+alert("warning");
+document.writeln("IM Ro fan");
+document.write("45 only");
+
+//inpute
+a = prompt("Enter ur name");
