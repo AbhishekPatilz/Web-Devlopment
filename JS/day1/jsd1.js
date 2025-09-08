9251cbb7ecf0fee8b65aef3d7eaf218339c86375
AbhishekPatilz <ap3820214a@gmail.com>
2025-09-09 00:43:27 +0530

    done

--- JS/day1/jsd1.js
+++ JS/day1/jsd1.js
@@ -5,14 +5,14 @@
 // // let c = a + b;
 // console.log(a+b);
 
-console.log("Hello World");
-confirm("take all cooke");
-alert("warning");
-document.writeln("IM Ro fan");
-document.write("45 only");
+// console.log("Hello World");
+// confirm("take all cooke");
+// alert("warning");
+// document.writeln("IM Ro fan");
+// document.write("45 only");
 
 //inpute
-a = prompt("Enter ur name");
+prompt("Enter ur name");
 
 // token-is smallest entity of program
 //token== datatype/keyword + variable/identifire + value/constant
@@ -23,10 +23,10 @@ let c = a + b;
 console.log(a+b);
 
 console.log("Hello World");
-confirm("take all cooke");
-alert("warning");
+confirm(a+b);
+alert(a+b);
 document.writeln("IM Ro fan");
 document.write("45 only");
 
 //inpute
-a = prompt("Enter ur name");
+a = prompt(a+b);
